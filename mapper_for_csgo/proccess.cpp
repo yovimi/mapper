02d25e19760cd44310c8eb2849512e8fa61b890f
yovimi <76208374+yovimi@users.noreply.github.com>
2023-01-10 08:58:02 -0800

    add loading system dlls (for imports)

--- mapper_for_csgo/proccess.cpp
+++ mapper_for_csgo/proccess.cpp
@@ -23,16 +23,25 @@ namespace game
 				game::process_id = ProcessEntry.th32ProcessID;
 				game::process = OpenProcess(PROCESS_ALL_ACCESS, false, ProcessEntry.th32ProcessID);
 
+				PVOID dll = VirtualAllocEx(game::process, NULL, strlen(("VCRUNTIME140.dll")), MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+				WriteProcessMemory(game::process, dll, ("VCRUNTIME140.dll"), strlen(("VCRUNTIME140.dll")), NULL);
+				LPVOID loadlib = (LPVOID)GetProcAddress(GetModuleHandle(("kernel32.dll")), ("LoadLibraryA"));
+				HANDLE remote = CreateRemoteThread(game::process, NULL, NULL, (LPTHREAD_START_ROUTINE)loadlib, dll, NULL, NULL);
+
 				if (!game::process)
 				{
 					ERROR("Failed attach to proccess");
 				}
-
 				return true;
 			}
 		}
 
 		CloseHandle(Toolhelp);
 		return false;
+	}
+
+	void load_sys_dll(const char* module)
+	{
+
 	}
 }
